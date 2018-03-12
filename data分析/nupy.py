0066f00312abf7ba7d6afa58aa4ebe07a2e98143
wuxy <wuxy@bitland.corp-partner.google.com>
2018-03-12 14:07:58 +0800

    matplotlib&scipy&numpy

---
+++ data分析/nupy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*
+
+import numpy as np
+
+a = np.array([2,0,1,5])
+print(a)
+print(a[:3])
+print(a.min())
+a.sort()
+print(a)
+print('----')
+b = np.array([[1,2,3],[4,5,6]])
+
+print(b*b)
---
+++ data分析/scipytest.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*
+
+from scipy.optimize import fsolve
+
+
+def f(x):
+	x1 = x[0]
+	x2 = x[1]
+	return [2*x1 - x2**2 -1, x1**2 - x2 -2]
+
+result = fsolve(f,[1,1])
+print(result)
+
+
